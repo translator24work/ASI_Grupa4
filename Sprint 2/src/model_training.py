c07f34e0c0144fa521a376430b6335d97efebf88
translator24work <translatorwork24@gmail.com>
2023-10-17 18:51:58 +0200

    Sprint2

---
+++ Sprint 2/src/model_training.py
@@ -0,0 +1,6 @@
+from sklearn.ensemble import RandomForestRegressor
+
+def train_model(X_train, y_train):
+    model = RandomForestRegressor(random_state=42)
+    model.fit(X_train, y_train)
+    return model
