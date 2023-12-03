3e93ed161cd0a88a3c27db1fa20cca199f8a6dbc
perlowicz <bartek.perlinski1234@gmail.com>
2023-12-03 19:23:52 +0100

    Initalize kedro project

--- Sprint 2/src/model_training.py
+++
@@ -1,6 +0,0 @@
-from sklearn.ensemble import RandomForestRegressor
-
-def train_model(X_train, y_train):
-    model = RandomForestRegressor(random_state=42)
-    model.fit(X_train, y_train)
-    return model
