bfeff5b347fdb0f4498a4b3e90e901c70e950a50
s19792 Hubert Gontarz <s19792@pjwstk.edu.pl>
2024-01-09 00:03:03 +0100

    Change StreamLit implementation

---
+++ kedro-asi/streamlit.py
@@ -0,0 +1,17 @@
+import streamlit as st
+from kedro.framework.startup import bootstrap_project
+from kedro.framework.session import KedroSession
+from pathlib import Path
+
+
+# Załadowanie kontekstu Kedro
+project_path = Path.cwd()
+bootstrap_project(project_path)
+
+
+with KedroSession.create(project_path) as session:
+    st.title('Kedro pipeline')
+
+    if st.button('Start Kedro Pipeline'):
+        session.run(pipeline_name="__default__")
+        st.success('Pipeline has been started successfully!')
