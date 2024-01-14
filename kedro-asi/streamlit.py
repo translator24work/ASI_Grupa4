c4ead47e5287f70496a8024b256bc134617899a3
perlowicz <bartek.perlinski1234@gmail.com>
2024-01-14 22:59:34 +0100

    Correct streamlit impl and introduce Faker library to generate some synthetic data

--- kedro-asi/streamlit.py
+++ kedro-asi/streamlit.py
@@ -1,18 +1,40 @@
 import streamlit as st
 from kedro.framework.startup import bootstrap_project
 from kedro.framework.session import KedroSession
-from sqlalchemy import create_engine
 from pathlib import Path
+from faker import Faker
 import pandas as pd
+from autogluon.tabular import TabularPredictor
 import numpy as np
-import joblib
 
-# Load your trained model
-model = joblib.load('AutogluonModels/ag-20231213_182836/predictor.pkl')
+
+model_path = max(Path('AutogluonModels').iterdir(), key=lambda x: x.stat().st_mtime)
+fake = Faker()
 
 # Załadowanie kontekstu Kedro
 project_path = Path.cwd()
 bootstrap_project(project_path)
+
+
+def generate_synthetic_data(num_samples):
+    synthetic_data = {
+        "world_rank": np.random.randint(1, 500, num_samples),
+        "institution": [fake.company() for _ in range(num_samples)],
+        "country": [fake.country() for _ in range(num_samples)],
+        "national_rank": np.random.randint(1, 100, num_samples),
+        "quality_of_education": np.random.randint(1, 100, num_samples),
+        "alumni_employment": np.random.randint(1, 100, num_samples),
+        "quality_of_faculty": np.random.randint(1, 100, num_samples),
+        "publications": np.random.randint(1, 100, num_samples),
+        "influence": np.random.randint(1, 100, num_samples),
+        "citations": np.random.randint(1, 100, num_samples),
+        "broad_impact": np.random.randint(1, 100, num_samples),
+        "patents": np.random.randint(1, 100, num_samples),
+        "score": np.random.uniform(0, 100, num_samples),
+        "year": np.random.randint(2000, 2024, num_samples)
+    }
+    return pd.DataFrame(synthetic_data)
+
 
 with KedroSession.create(project_path) as session:
     st.title('Kedro pipeline')
@@ -23,8 +45,8 @@ with KedroSession.create(project_path) as session:
 
     with st.form(key='predict_form'):
         world_rank = st.number_input('World Rank', min_value=1)
-        institution = st.text_input('Institution', value="")
-        country = st.text_input('Country', value="")
+        institution = st.text_input('Institution', value="Harvard University")
+        country = st.text_input('Country', value="USA")
         national_rank = st.number_input('National Rank', min_value=1)
         quality_of_education = st.number_input('Quality of Education', min_value=1)
         alumni_employment = st.number_input('Alumni Employment', min_value=1)
@@ -36,6 +58,7 @@ with KedroSession.create(project_path) as session:
         patents = st.number_input('Patents', value=1)
         score = st.number_input('Score', value=1)
         year = st.number_input('Year', value=1)
+
         submit_button = st.form_submit_button('Make a Prediction')
 
     if submit_button:
@@ -57,31 +80,16 @@ with KedroSession.create(project_path) as session:
             'year': year
         }
         data_to_predict_df = pd.DataFrame([data_to_predict])
-        prediction = model.predict(data_to_predict_df)
-        st.success(f'Prediction Score: {prediction}')
 
+        # Make a prediction
+        predictor = TabularPredictor.load(str(model_path))
+        prediction = predictor.predict(data_to_predict_df)
 
-def generate_synthetic_data(sample_count):
-    synthetic_data = {
-        "world_rank": np.random.randint(1, 500, num_samples),
-        "institution": [f"Institution {i}" for i in range(num_samples)],
-        "country": ["Country " + chr(65 + i % 26) for i in range(num_samples)],
-        "national_rank": np.random.randint(1, 100, num_samples),
-        "quality_of_education": np.random.randint(1, 100, num_samples),
-        "alumni_employment": np.random.randint(1, 100, num_samples),
-        "quality_of_faculty": np.random.randint(1, 100, num_samples),
-        "publications": np.random.randint(1, 100, num_samples),
-        "influence": np.random.randint(1, 100, num_samples),
-        "citations": np.random.randint(1, 100, num_samples),
-        "broad_impact": np.random.randint(1, 100, num_samples),
-        "patents": np.random.randint(1, 100, num_samples),
-        "score": np.random.uniform(0, 100, num_samples),
-        "year": np.random.randint(2000, 2024, num_samples)
-    }
-    return pd.DataFrame(synthetic_data)
+        # Display the prediction
+        st.success(f'Prediction Score: {prediction.iloc[0]}')
 
-
-num_samples = st.sidebar.number_input("Enter the number of synthetic samples to generate", min_value=1, max_value=1000, value=10)
-if st.sidebar.button('Generate Synthetic Data'):
-    synthetic_data_df = generate_synthetic_data(num_samples)
-    st.write(synthetic_data_df)
+    num_samples = st.sidebar.number_input("Enter the number of synthetic samples to generate", min_value=1,
+                                          max_value=1000, value=10)
+    if st.sidebar.button('Generate Synthetic Data'):
+        synthetic_data_df = generate_synthetic_data(num_samples)
+        st.write(synthetic_data_df)
