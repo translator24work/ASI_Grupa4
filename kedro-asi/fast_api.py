cdd41d22aa9e867d0ef7205dad1d0fd7a776dee2
perlowicz <bartek.perlinski1234@gmail.com>
2024-01-29 14:59:19 +0100

    Add fast_api application and make some improvements in streamlit + add synthetic data table into sql script

---
+++ kedro-asi/fast_api.py
@@ -0,0 +1,116 @@
+from fastapi import FastAPI, HTTPException, Request
+from autogluon.tabular import TabularPredictor
+from kedro.framework.startup import bootstrap_project
+from kedro.framework.session import KedroSession
+from kedro.config import ConfigLoader
+import pandas as pd
+from pathlib import Path
+from sqlalchemy import create_engine
+from sdv.lite import SingleTablePreset
+from sdv.metadata import SingleTableMetadata
+
+app = FastAPI()
+DATABASE_SCHEMA_NAME = 'cwur'
+SYNTHETIC_DATA_TABLE_NAME = 'synthetic_cwur'
+PRODUCTION_DATA_TABLE_NAME = 'cwur'
+
+
+@app.get('/run')
+async def run_pipeline():
+    try:
+        project_path = Path.cwd()
+        bootstrap_project(project_path)
+        with KedroSession.create(project_path) as session:
+            session.run(pipeline_name="__default__")
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+
+@app.post('/predict')
+async def make_prediction(request: Request):
+    try:
+        global predictor
+        model_directory = Path('AutogluonModels')
+        if not model_directory.exists():
+            raise FileNotFoundError(f"Could not find directory with Autogluon Models: {model_directory}")
+        latest_model_path = max(model_directory.iterdir(), key=lambda x: x.stat().st_mtime)
+        if not latest_model_path.is_dir():
+            raise FileNotFoundError(f"Could not find specific model: {latest_model_path}")
+        print('Znalazłem model poprawnie')
+        input_data = await request.json()
+        data = pd.DataFrame([input_data])
+        predictor = TabularPredictor.load(str(latest_model_path))
+        prediction = predictor.predict(data)
+        return {"prediction": prediction.tolist()}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+
+@app.get('/predictSynthetic')
+async def make_prediction_for_synthetic():
+    try:
+        global predictor
+        model_directory = Path('AutogluonModels')
+        if not model_directory.exists():
+            raise FileNotFoundError(f"Could not find directory with Autogluon Models: {model_directory}")
+        latest_model_path = max(model_directory.iterdir(), key=lambda x: x.stat().st_mtime)
+        if not latest_model_path.is_dir():
+            raise FileNotFoundError(f"Could not find specific model: {latest_model_path}")
+        created_predictor = TabularPredictor.load(str(latest_model_path))
+        conf_loader = ConfigLoader(conf_source=str(Path.cwd() / 'conf'))
+        credentials = conf_loader.get("local/credentials", "credentials.yml")
+        db_username = credentials["postgres"]["username"]
+        db_password = credentials["postgres"]["password"]
+        db_host = credentials["postgres"]["host"]
+        db_port = credentials["postgres"]["port"]
+        database_name = credentials["postgres"]["database"]
+        user = db_username
+        password = db_password
+        host = db_host
+        database = database_name
+        connection_string = f"postgresql://{user}:{password}@{host}:{db_port}/{database}"
+        engine = create_engine(connection_string)
+        synthetic_data = pd.read_sql_table(SYNTHETIC_DATA_TABLE_NAME, engine, DATABASE_SCHEMA_NAME)
+        prediction = created_predictor.predict(synthetic_data)
+        return {"prediction": prediction.tolist()}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+
+@app.post('/synthetic')
+async def generate_synthetic_data(request: Request):
+    try:
+        number_of_rows = await request.json()
+        metadata = SingleTableMetadata()
+        conf_loader = ConfigLoader(conf_source=str(Path.cwd() / 'conf'))
+        credentials = conf_loader.get("local/credentials", "credentials.yml")
+        db_username = credentials["my_postgres_db"]["username"]
+        db_password = credentials["my_postgres_db"]["password"]
+        db_host = credentials["my_postgres_db"]["host"]
+        db_port = credentials["my_postgres_db"]["port"]
+        database_name = credentials["my_postgres_db"]["database"]
+        user = db_username
+        password = db_password
+        host = db_host
+        database = database_name
+        connection_string = f"postgresql://{user}:{password}@{host}:{db_port}/{database}"
+        engine = create_engine(connection_string)
+        real_data = pd.read_sql_table(PRODUCTION_DATA_TABLE_NAME,
+                                      engine,
+                                      DATABASE_SCHEMA_NAME)
+        metadata.detect_from_dataframe(real_data)
+        metadata.update_column(column_name='institution', sdtype='categorical')
+
+        synthesizer = SingleTablePreset(metadata, name='FAST_ML')
+        synthesizer.fit(data=real_data)
+        synthetic_data = synthesizer.sample(num_rows=number_of_rows['number_of_rows'])
+        synthetic_data.to_sql(SYNTHETIC_DATA_TABLE_NAME,
+                              engine,
+                              DATABASE_SCHEMA_NAME,
+                              if_exists='append',
+                              index=False)
+
+        return {"synthetic": synthetic_data.to_json()}
+    except Exception as e:
+        print(str(e))
+        raise HTTPException(status_code=500, detail=str(e))
--- kedro-asi/streamlit.py
+++ kedro-asi/streamlit.py
@@ -1,95 +1,94 @@
-import streamlit as st
-from kedro.framework.startup import bootstrap_project
-from kedro.framework.session import KedroSession
 from pathlib import Path
-from faker import Faker
 import pandas as pd
-from autogluon.tabular import TabularPredictor
-import numpy as np
-
+import requests
+import streamlit as st
 
 model_path = max(Path('AutogluonModels').iterdir(), key=lambda x: x.stat().st_mtime)
-fake = Faker()
-
-# Załadowanie kontekstu Kedro
-project_path = Path.cwd()
-bootstrap_project(project_path)
-
-
-def generate_synthetic_data(num_samples):
-    synthetic_data = {
-        "world_rank": np.random.randint(1, 500, num_samples),
-        "institution": [fake.company() for _ in range(num_samples)],
-        "country": [fake.country() for _ in range(num_samples)],
-        "national_rank": np.random.randint(1, 100, num_samples),
-        "quality_of_education": np.random.randint(1, 100, num_samples),
-        "alumni_employment": np.random.randint(1, 100, num_samples),
-        "quality_of_faculty": np.random.randint(1, 100, num_samples),
-        "publications": np.random.randint(1, 100, num_samples),
-        "influence": np.random.randint(1, 100, num_samples),
-        "citations": np.random.randint(1, 100, num_samples),
-        "broad_impact": np.random.randint(1, 100, num_samples),
-        "patents": np.random.randint(1, 100, num_samples),
-        "score": np.random.uniform(0, 100, num_samples),
-        "year": np.random.randint(2000, 2024, num_samples)
-    }
-    return pd.DataFrame(synthetic_data)
+SYNTHETIC_DATA_URL = "http://localhost:8000/synthetic"
+RUN_PIPELINE_URL = "http://localhost:8000/run"
+PREDICT_URL = "http://localhost:8000/predict"
+PREDICT_URL_SYNTHETIC = "http://localhost:8000/predictSynthetic"
 
 
-with KedroSession.create(project_path) as session:
-    st.title('Kedro pipeline')
+st.title('Streamlit app designed for working with Kedro pipelines ')
 
-    if st.button('Start Kedro Pipeline'):
-        session.run(pipeline_name="__default__")
-        st.success('Pipeline has been started successfully!')
-
-    with st.form(key='predict_form'):
-        world_rank = st.number_input('World Rank', min_value=1)
-        institution = st.text_input('Institution', value="Harvard University")
-        country = st.text_input('Country', value="USA")
-        national_rank = st.number_input('National Rank', min_value=1)
-        quality_of_education = st.number_input('Quality of Education', min_value=1)
-        alumni_employment = st.number_input('Alumni Employment', min_value=1)
-        quality_of_faculty = st.number_input('Quality of Faculty', min_value=1)
-        publications = st.number_input('Publications', min_value=1)
-        influence = st.number_input('Influence', value=1)
-        citations = st.number_input('Citations', value=1)
-        broad_impact = st.number_input('Broad Impact', value=1)
-        patents = st.number_input('Patents', value=1)
-        score = st.number_input('Score', value=1)
-        year = st.number_input('Year', value=1)
-
-        submit_button = st.form_submit_button('Make a Prediction')
-
-    if submit_button:
-        # preparing data for prediction
-        data_to_predict = {
-            'world_rank': world_rank,
-            'institution': institution,
-            'country': country,
-            'national_rank': national_rank,
-            'quality_of_education': quality_of_education,
-            'alumni_employment': alumni_employment,
-            'quality_of_faculty': quality_of_faculty,
-            'publications': publications,
-            'influence': influence,
-            'citations': citations,
-            'broad_impact': broad_impact,
-            'patents': patents,
-            'score': score,
-            'year': year
-        }
-        data_to_predict_df = pd.DataFrame([data_to_predict])
-
-        # Make a prediction
-        predictor = TabularPredictor.load(str(model_path))
-        prediction = predictor.predict(data_to_predict_df)
-
-        # Display the prediction
-        st.success(f'Prediction Score: {prediction.iloc[0]}')
-
-    num_samples = st.sidebar.number_input("Enter the number of synthetic samples to generate", min_value=1,
+synthetic_data_amount = st.sidebar.number_input("Enter the number of synthetic samples to generate", min_value=1,
                                           max_value=1000, value=10)
-    if st.sidebar.button('Generate Synthetic Data'):
-        synthetic_data_df = generate_synthetic_data(num_samples)
-        st.write(synthetic_data_df)
+
+if st.button('Start Kedro Pipeline'):
+    response = requests.get(RUN_PIPELINE_URL)
+    st.success('Kedro pipeline has been started')
+    if response.status_code == 200:
+        st.success('Kedro pipeline ended successfully!')
+    else:
+        st.error('There was some error while running Kedro pipeline')
+
+
+with st.form(key='predict_form'):
+    world_rank = st.number_input('World Rank', min_value=1)
+    institution = st.text_input('Institution', value="Harvard University")
+    country = st.text_input('Country', value="USA")
+    national_rank = st.number_input('National Rank', min_value=1)
+    quality_of_education = st.number_input('Quality of Education', min_value=1)
+    alumni_employment = st.number_input('Alumni Employment', min_value=1)
+    quality_of_faculty = st.number_input('Quality of Faculty', min_value=1)
+    publications = st.number_input('Publications', min_value=1)
+    influence = st.number_input('Influence', value=1)
+    citations = st.number_input('Citations', value=1)
+    broad_impact = st.number_input('Broad Impact', value=1)
+    patents = st.number_input('Patents', value=1)
+    score = st.number_input('Score', value=1)
+    year = st.number_input('Year', value=1)
+
+    submit_button = st.form_submit_button('Make a Prediction')
+
+
+if submit_button:
+    data_to_predict = {
+        'world_rank': world_rank,
+        'institution': institution,
+        'country': country,
+        'national_rank': national_rank,
+        'quality_of_education': quality_of_education,
+        'alumni_employment': alumni_employment,
+        'quality_of_faculty': quality_of_faculty,
+        'publications': publications,
+        'influence': influence,
+        'citations': citations,
+        'broad_impact': broad_impact,
+        'patents': patents,
+        'score': score,
+        'year': year
+    }
+
+    response = requests.post(PREDICT_URL, json=data_to_predict)
+
+    if response.status_code == 200:
+        prediction = response.json()
+        st.success(f"Prediction: {prediction['prediction']}")
+    else:
+        st.error(f'There was some error while making prediction: {response}')
+
+
+if st.sidebar.button('Generate Synthetic data'):
+    synthetic_data_amount_json = {
+        "number_of_rows": synthetic_data_amount
+    }
+    response = requests.post(SYNTHETIC_DATA_URL, json=synthetic_data_amount_json)
+    if response.status_code == 200:
+        st.success('Synthetic data has been generated')
+        data = response.json()
+        synthetic_data = pd.read_json(data['synthetic'])
+        st.dataframe(synthetic_data)
+    else:
+        st.error('There was some error while creating synthetic data')
+        st.write(response)
+
+
+if st.sidebar.button('Make Prediction for synthetic data'):
+    response = requests.get(PREDICT_URL_SYNTHETIC)
+    if response.status_code == 200:
+        prediction = response.json()
+        st.success(f"Prediction for synthetic data: {prediction['prediction']}")
+    else:
+        st.error('There was some error while making prediction for synthetic data')
